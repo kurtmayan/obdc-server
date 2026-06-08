814a890c57ffb617660ee460b7859f943788079b
John Laurence Burgos <laurence@mayan.com.ph>
2026-06-08 16:49:28 +0800

    remove console.logs

--- src/modules/queue/queue.processor.ts
+++ src/modules/queue/queue.processor.ts
@@ -123,11 +123,6 @@ export class TaskProcessor extends WorkerHost {
         for (const log of record.attendance_record) {
           if (existingAttendanceSet.has(log.id)) continue;
 
-          console.log('---------------------------------------------------');
-          console.log(log.log_date);
-          console.log(new Date(log.log_date + '.000Z'));
-          console.log('---------------------------------------------------');
-
           batch.push({
             id: log.id,
             employeeName: log.employee_name,
