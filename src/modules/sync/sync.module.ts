4cd6b200f746bdf50c36f29180d53943e43fb7c8
ItsYahBoiLaurence <laurence@mayan.com.ph>
2026-07-13 17:44:50 +0800

    wire sqs module

--- src/modules/sync/sync.module.ts
+++ src/modules/sync/sync.module.ts
@@ -3,10 +3,11 @@ import { SyncService } from './sync.service';
 import { SyncController } from './sync.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { QueueModule } from '../queue/queue.module';
+import { SqsQueueModule } from '../sqs-queue/sqs-queue.module';
 
 @Module({
   imports: [QueueModule],
-  providers: [SyncService, PrismaService],
+  providers: [SyncService, PrismaService, SqsQueueModule],
   controllers: [SyncController],
 })
 export class SyncModule {}
