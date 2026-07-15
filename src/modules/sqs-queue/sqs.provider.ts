e50f8fdb4f589655e1fb56c967d0545d956a713b
ItsYahBoiLaurence <laurence@mayan.com.ph>
2026-07-15 18:44:47 +0800

    remove provider linking

--- src/modules/sqs-queue/sqs.provider.ts
+++ src/modules/sqs-queue/sqs.provider.ts
@@ -9,12 +9,12 @@ export const SqsClientProvider: Provider = {
   useFactory: (configService: ConfigService): SQSClient => {
     return new SQSClient({
       region: configService.getOrThrow<string>('AWS_REGION'),
-      credentials: {
-        accessKeyId: configService.getOrThrow<string>('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: configService.getOrThrow<string>(
-          'AWS_SECRET_ACCESS_KEY',
-        ),
-      },
+      // credentials: {
+      //   accessKeyId: configService.getOrThrow<string>('AWS_ACCESS_KEY_ID'),
+      //   secretAccessKey: configService.getOrThrow<string>(
+      //     'AWS_SECRET_ACCESS_KEY',
+      //   ),
+      // },
     });
   },
 };
