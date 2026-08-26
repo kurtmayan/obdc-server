360487598bacc2a339463fdeb260ae62529958db
lindsey-mayan <lindsey@mayan.com.ph>
2026-08-26 17:00:45 +0800

    update MyHr module to include PrismaService and SqsQueueModule

--- src/modules/myhr/myhr.module.ts
+++ src/modules/myhr/myhr.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MyHrService } from './myhr.service';
 import { MyHrController } from './myhr.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { SqsQueueModule } from '../sqs-queue/sqs-queue.module';
 
 @Module({
+  imports: [SqsQueueModule],
   controllers: [MyHrController],
-  providers: [MyHrService],
+  providers: [MyHrService, PrismaService],
 })
 export class MyhrModule {}
