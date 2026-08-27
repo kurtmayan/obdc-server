c6dd101ec654b35d073de16706d2b44047de961f
lindsey-mayan <lindsey@mayan.com.ph>
2026-08-27 14:42:24 +0800

    remove console log for message type handling in SqsProcessor

--- src/modules/sqs-queue/sqs.processor.ts
+++ src/modules/sqs-queue/sqs.processor.ts
@@ -150,7 +150,7 @@ export class SqsProcessor
 
   private async handleMessage(message: AppQueueMessage): Promise<void> {
     const type = message.type;
-    console.log(`Handling message of type: ${type}`);
+
     switch (message.type) {
       case 'SYNC_RECORDS':
         await this.processSyncRecords(message.payload);
